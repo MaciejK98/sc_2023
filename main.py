74742dae6b8d0a257b572464797350cb1eeb8425
MaciejK98 <maciomacio110@gmail.com>
2023-05-27 19:41:09 +0200

    users working

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 
-
+import time
 from src.generator1 import generator
 from src.Network import Network 
+from src.User import User
 # from src.User import User 
 # from src.Process import Process 
 # from .src import src
 generator(1)
 
 network = Network(1,1,1,1,1)
+user1 = User(111,50,3)
+i=0
+while user1.Location<3001:
+  # print ("Silmulation goes brrr")
+  i+=1
+  # print(user1.ReportPower())
+  # user1.ChangePosition()
+  # print("              location", user1.Location)
+  user1.execute()
+  # time.sleep(0.1)
+  # print(user1.Location)
+  # break
 
-while True:
-  print ("Silmulation goes brrr")
+print(i)
 # import src.Process
 # import src.lab1
--- src/User.py
+++ src/User.py
@@ -2,17 +2,24 @@ import random
 from .Process import Process
 import numpy as np
 
+
 # class User(Process):
 class User():
     # ConnectedBaseStation= "A"
     # DistanceToBaseStation= 2000
     
-    def __init__(self, user_id,speed):
+    def __init__(self, user_id,speed,alpha):
         self.UserId = user_id  # unikalny identyfikator użytkownika
         self.ConnectedBaseStation= 'Station A'
-        self.Location= 2000 #meters
+        self.Location= 2000.0 #meters
         self.MOVEMENTSPEED= speed
         self.WaitTime= 20 #ms
+        self.BaseStationAPower = 0
+        self.BaseStationBPower = 0
+        self.alpha = alpha # optimization of this parameter
+        self.delta = 20 # db difference to disconnect the user
+        self.state = 1
+        self.ttt =0 # 100ms -> 5 times
         
     def GenerateSpeed(self): 
         return np.random(5,50) #change to proper generator
@@ -20,14 +27,19 @@ class User():
     def ChangePosition(self):
         self.Location+=self.MOVEMENTSPEED*self.WaitTime/1000
         
-    def PwrReceived(self):
-        # print("Received Power From Base Station")
+    def PwrBSA(self):
+        # Received Power From Base Station
         s = random.gauss(0, 4)
-        CalculatedPower= {}
-        CalculatedPower['Station A'] =4.56 - 22*np.log10(self.Location)+ s# + random
-        CalculatedPower['Station B'] =4.56 - 22*np.log10(5000-self.Location)+ s# + random
+        self.BaseStationAPower =4.56 - 22*np.log10(self.Location)+ s# + random
 
-        return CalculatedPower
+        return self.BaseStationAPower
+    
+    def PwrBSB(self):
+        # Received Power From Base Station
+        s = random.gauss(0, 4)
+        self.BaseStationBPower =4.56 - 22*np.log10(5000-self.Location)+ s# + random
+
+        return self.BaseStationBPower
     
     def CommitHandOver(self):
         if self.ConnectedBaseStation== 'Station A':
@@ -36,8 +48,56 @@ class User():
             self.ConnectedBaseStation= 'Station A'
     
     def ReportPower(self):
-        report = ""
-        for station_id, power in self.PwrReceived.items():
-            report += f"Station {station_id}: {power} dBm\n"
+        report = f"Station A: {self.PwrBSA()} dBm\n"
+        report += f"Station B: {self.PwrBSB()} dBm\n"
+        report += f"position:  {self.Location}) "
+        report += f"BS: {self.ConnectedBaseStation}"
+
         return report
         
+    def disconnect(self):
+        print("disconnected")
+        pass
+    
+    def execute(self):
+        active = True
+        while active:
+            
+            self.ChangePosition()
+            print( self.ReportPower())
+            # print(self.state)
+            
+            match self.state:
+                case 1: #connected to BS-A
+                    if self.BaseStationAPower - self.BaseStationBPower > self.delta: #drop user
+                        self.disconnect()
+                        
+                    if self.BaseStationBPower - self.BaseStationAPower > self.alpha:
+                        self.ttt +=1
+                        # print ("różnica mocy:    ", self.BaseStationBPower-self.BaseStationAPower)
+                                                
+                        if self.ttt >= 5:
+                            # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                            self.CommitHandOver()
+                            self.ttt = 0
+                            self.state = 2
+                    else:
+                        self.ttt = 0
+                    
+                case 2: #connected to BS-B
+                    if self.BaseStationBPower - self.BaseStationAPower > self.delta: #drop user
+                        # print ("różnica mocy:    ", self.BaseStationBPower-self.BaseStationAPower)
+                        self.disconnect()
+                    
+                    if self.BaseStationAPower - self.BaseStationBPower > self.alpha:
+                        self.ttt +=1
+                        # print ("różnica mocy:    ", self.BaseStationAPower-self.BaseStationBPower)
+                        
+                        if self.ttt >= 5:
+                            # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                            self.CommitHandOver()
+                            self.ttt = 0
+                            self.state = 1
+                    else:
+                        self.ttt = 0
+            break
